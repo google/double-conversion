a711666ddd063eb1e4b181a6cb981d39a1fc8bac
Isaac Hier <isaachier@gmail.com>
2017-09-05 19:55:14 -0400

    Update CMake package generation.

--- double-conversion/cached-powers.cc
+++ double-conversion/cached-powers.cc
@@ -29,9 +29,9 @@
 #include <limits.h>
 #include <math.h>
 
-#include "utils.h"
+#include <double-conversion/utils.h>
 
-#include "cached-powers.h"
+#include <double-conversion/cached-powers.h>
 
 namespace double_conversion {
 
