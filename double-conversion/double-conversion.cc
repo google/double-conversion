1d5a688277446296dd67c8495397b4880ce8610c
uburuntu <bekbulatov.ramzan@ya.ru>
2017-09-14 21:19:09 +0300

    REF: replace deprecated headers

--- double-conversion/double-conversion.cc
+++ double-conversion/double-conversion.cc
@@ -25,9 +25,9 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#include <limits.h>
+#include <climits>
 #include <locale>
-#include <math.h>
+#include <cmath>
 
 #include <double-conversion/double-conversion.h>
 
