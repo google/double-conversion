7e686e77398c7692a5581f04891a2d2b2d7784a9
Friedemann Kleint <Friedemann.Kleint@qt.io>
2019-12-19 10:24:55 +0100

    Limit MSVC-specific #proagma use to versions up to 2012
    The optimisation-control on IsDecimalDigitForRadix(), to avoid a
    warning, broke clang-cl builds (unknown #pragma); so refine the #if
    condition to only apply it for versions up to MSVC2012, the one
    documented to need it.
    
    Mention the significance of suppressing optimisation in the comment
    explaining it.  Also fix the grammar of the last sentence.  (The "By
    moving it ..."  prefix on a sentence with "the compiler" as subject
    tacitly claimed the compiler was doing the moving.)

--- double-conversion/string-to-double.cc
+++ double-conversion/string-to-double.cc
@@ -42,6 +42,9 @@
 // against Advance and friends, when instantiated with **it as char, not uc16.
  __pragma(warning(disable: 4244))
 #  endif
+#  if _MSC_VER <= 1700 // VS2012, see IsDecimalDigitForRadix warning fix, below
+#    define VS2012_RADIXWARN
+#  endif
 #endif
 
 namespace double_conversion {
@@ -158,9 +161,9 @@ static double SignedZero(bool sign) {
 //
 // The function is small and could be inlined, but VS2012 emitted a warning
 // because it constant-propagated the radix and concluded that the last
-// condition was always true. By moving it into a separate function the
-// compiler wouldn't warn anymore.
-#ifdef _MSC_VER
+// condition was always true. Moving it into a separate function and
+// suppressing optimisation keeps the compiler from warning.
+#ifdef VS2012_RADIXWARN
 #pragma optimize("",off)
 static bool IsDecimalDigitForRadix(int c, int radix) {
   return '0' <= c && c <= '9' && (c - '0') < radix;
